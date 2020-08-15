d0f537a06e547c9620c480ccd7daa2044ef667b0
guilhermeaw <guilherme.w@universo.univates.br>
2020-08-14 21:50:02 -0300

    Connected to API

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: string;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
           <header>
-            <img src="https://avatars1.githubusercontent.com/u/59237399?s=460&u=8df812cfa0cfe0373e62e95831ff7721d431e6a0&v=4" alt="Guilherme"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Guilherme</strong>
-              <span>Química</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
 
-          <p>
-            Entusiasta das melhores tecnologias
-            <br /><br />
-            Entusiasta das melhores tecnologias Entusiasta das melhores tecnologias Entusiasta das melhores tecnologias Entusiasta das melhores tecnologias 
-          </p>
+          <p>{teacher.bio}</p>
 
           <footer>
             <p>
               Preço/hora
-              <strong>R$ 200,00</strong>
+              <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              onClick={createNewConnection} 
+              href={`https://wa.me/${teacher.whatsapp}`}
+            >
               <img src={whatsappIcon} alt="whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   );
